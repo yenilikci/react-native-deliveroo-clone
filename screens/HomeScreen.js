e740c1f336e65202e70cc96f9223cf4dd9051364
yenilikci <57464067+yenilikci@users.noreply.github.com>
2022-10-18 20:32:37 +0300

    add hero icons and header

--- screens/HomeScreen.js
+++ screens/HomeScreen.js
@@ -1,10 +1,46 @@
-import { View, Text } from 'react-native'
-import React from 'react'
+import { View, Text, Image } from 'react-native'
+import React, { useLayoutEffect } from 'react'
+import { useNavigation } from '@react-navigation/native'
+import { SafeAreaView } from 'react-native-safe-area-context';
+import {
+    ChevronDownIcon, UserIcon
+} from 'react-native-heroicons/outline'
+
 
 export default function HomeScreen() {
-  return (
-    <View>
-      <Text className="text-red-400">HomeScreen</Text>
-    </View>
-  )
+
+    const navigation = useNavigation();
+
+    useLayoutEffect(() => {
+        navigation.setOptions({
+            headerShown: false
+        })
+
+        return () => {
+
+        };
+    }, [])
+
+    return (
+        <SafeAreaView className="bg-white pt-5">
+                {/* Header */}
+                <View className="flex-row pb-3 items-center mx-4 space-x-2">
+                    <Image
+                        source={{
+                            uri: 'https://links.papareact.com/wru'
+                        }}
+                        className="h-7 w-7 bg-gray-300 p-4 rounded-full"
+                    />
+                    <View className="flex-1">
+                        <Text className="font-bold text-gray-400 text-xs">Deliver Now!</Text>
+                        <Text className="font-bold text-xl">
+                            Current Location
+                            <ChevronDownIcon size={20} color="#00CCBB" />
+                        </Text>
+                    </View>
+                    <UserIcon size={35} color="#00CCBB" />
+                </View>
+
+        </SafeAreaView>
+    )
 }
